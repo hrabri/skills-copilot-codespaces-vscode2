2caf35399b457659367e51d17760bd91c72620e5
Hrabri <hrabri.rajic@intel.com>
2024-03-26 23:38:04 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,7 @@
+// Create web server
+// Create a web server that listens on port 3000 and serves the comments.html file
+// when someone visits http://localhost:3000/comments.
+
+// The comments.html file should contain a form that allows users to submit comments.
+// The form should have a textarea for the comment and a submit button.
+
